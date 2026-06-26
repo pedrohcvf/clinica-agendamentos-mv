799405344b2f89e335c97975b91b3595063b5c83
Pedro Carvalho <pedrohcvf1@gmail.com>
2026-06-26 20:39:30 -0300

    feat: initial project setup with Spring Boot

---
+++ src/main/java/com/pedro/clinica/ClinicaAgendamentosMvApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaAgendamentosMvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaAgendamentosMvApplication.class, args);
+	}
+
+}
